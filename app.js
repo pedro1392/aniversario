6c0f96079778ccb76b61316bca078edc39a41916
pedro1392 <133188030+pedro1392@users.noreply.github.com>
2023-05-11 01:00:57 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+function showModal() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "flex";
+    modal.style.visibility = "visible";
+    var video = document.getElementById("video");
+    video.play()
+    video.loop = true
+    var eel = document.getElementById("eel");
+    eel.style.visibility = "visible";
+    var duck = document.getElementById("duck");
+    duck.style.visibility = "visible";
+    var duck1 = document.getElementById("duck1");
+    duck1.style.visibility = "visible";
+    var boneco = document.getElementById("boneco");
+    boneco.style.visibility = "visible";
+    var turtle = document.getElementById("turtle");
+    turtle.style.visibility = "visible";
+  }
+
+
+    
